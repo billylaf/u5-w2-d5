793a81e3b9680a1e63d7b0501896e2474a6a4c0a
bilal lafdili <bilallafdili23@gmail.com>
2026-07-10 09:34:58 +0200

    Initial commit

---
+++ src/main/java/lafdilibilal/u5_w2_d5/entities/Dipendente.java
@@ -0,0 +1,17 @@
+package lafdilibilal.u5_w2_d5.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "dipendenti")
+public class Dipendente {
+    
+}
